69c25a5594f09cd3eda16551505d6854b639b398
aakashbh1 <aakashbhargavaga@gmail.com>
2023-07-09 21:17:37 +0530

    first commit

---
+++ src/data/projects.js
@@ -0,0 +1,144 @@
+const data = [
+  {
+    id: '1',
+    title: 'Movix',
+    description:
+      ' Discover personalized movie recommendations based on your preferences and interests.',
+    image: {
+      src: 'https://res.cloudinary.com/dx6tl6aa2/image/upload/v1671564532/portfolio/projects/Syntho/Screenshot_2022-12-20_at_21.26.17_1_c59hzo.jpg',
+      placeholderSrc:
+        'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAzIDIiPjwvc3ZnPg==',
+    },
+    links: {
+      site: '',
+    },
+    technologies: [
+      '#react',
+      '#redux',
+      '#html',
+      '#apis'
+    ],
+  },
+  {
+    id: '2',
+    title: 'Resume Builder',
+    description:
+      'Create professional resumes effortlessly. Customize templates, enter your information, and generate a polished resume in minutes. ',
+    image: {
+      src: 'https://res.cloudinary.com/dx6tl6aa2/image/upload/v1671563302/portfolio/projects/minfin/Screenshot_2022-12-20_at_21.06.43_vl5h6k.jpg',
+      placeholderSrc:
+        'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAzIDIiPjwvc3ZnPg==',
+    },
+    links: {
+      site: 'https://resumebuilderr.vercel.app/',
+      repo:'https://github.com/aakashbh1/Resume-Builder'
+    },
+    technologies: [
+      '#typescript',
+      '#react',
+      
+    ],
+  },
+  {
+    id: '3',
+    title: 'HooBank',
+    description:
+      'Introducing Hoobank: A cutting-edge bank webpage powered by React. Experience seamless navigation, secure transactions, and convenient account management.',
+    image: {
+      src: 'https://res.cloudinary.com/dx6tl6aa2/image/upload/v1671562261/portfolio/projects/fashion/Screenshot_2022-12-20_at_20.45.27_peyqly.jpg',
+      placeholderSrc:
+        'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAzIDIiPjwvc3ZnPg==',
+    },
+    links: {
+      site: 'https://hoo-bank-home-page-react.vercel.app/',
+      repo: 'https://https://github.com/aakashbh1/HooBank-HomePage-React',
+    },
+    technologies: [
+      '#typescript',
+      '#react',
+     
+    ],
+  },
+  {
+    id: '4',
+    title: 'Razorpay Clone',
+    description:
+      'Presenting a Razorpay clone website I developed, replicating the seamless payment gateway experience. ',
+    image: {
+      src: 'https://res.cloudinary.com/dx6tl6aa2/image/upload/v1628348072/portfolio/projects/react-marketplace/apiko-marketplace-lg_xppe9n.jpg',
+      placeholderSrc:
+        'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAzIDIiPjwvc3ZnPg==',
+    },
+    links: {
+      site: 'https://razor-pay-clone-o8ux.vercel.app/',
+      repo: 'https://github.com/aakashbh1/RazorPay-Clone',
+    },
+    technologies: [
+      '#react',
+      '#redux',
+      '#react-window',
+      '#restapi',
+    ],
+  },
+  {
+    id: '5',
+    title: 'Recipe WebApp',
+    description:
+      'Discover and save mouth-watering recipes, complete with ingredients, instructions, and beautiful images',
+    image: {
+      src: 'https://res.cloudinary.com/dx6tl6aa2/image/upload/v1628348070/portfolio/projects/mern-places/mern-places-lg_xf6zom.jpg',
+      placeholderSrc:
+        'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAzIDIiPjwvc3ZnPg==',
+    },
+    links: {
+      site: 'https://recipe-app-react1.vercel.app/',
+      repo: 'https://github.com/aakashbh1/Recipe-React-App',
+    },
+    technologies: [
+      '#react',
+      '#api',
+      '#local Storage'
+    ],
+  },
+  {
+    id: '6',
+    title: 'Rent Up',
+    description:
+      ' Browse and book vacation rentals worldwide, from cozy apartments to luxurious villas. Experience seamless travel planning with our user-friendly website, ',
+    image: {
+      src: 'https://res.cloudinary.com/dx6tl6aa2/image/upload/v1628348073/portfolio/projects/rn-shop/rn-shop-lg_fofglf.jpg',
+      placeholderSrc:
+        'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAzIDIiPjwvc3ZnPg==',
+    },
+    links: {
+      site: 'https://rentuptravel.vercel.app/',
+      repo: 'https://github.com/aakashbh1/Travel-Website-React',
+    },
+    technologies: [
+      'react',
+      'tailwind'
+    ],
+  },
+  {
+    id: '7',
+    title: 'Discord Clone',
+    description:
+      'A platform for seamless communication and collaboration. Enjoy real-time voice, video, and text chat with friends and communities. ',
+    image: {
+      src: 'https://res.cloudinary.com/dx6tl6aa2/image/upload/v1628348073/portfolio/projects/rn-places/rn-places-lg_ljyikm.jpg',
+      placeholderSrc:
+        'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAzIDIiPjwvc3ZnPg==',
+    },
+    links: {
+      site: 'https://discordclone1.vercel.app/',
+      repo: 'https://github.com/aakashbh1/Discord-Clone-',
+    },
+    technologies: [
+      'html',
+      'tailwind'
+    ],
+  },
+  
+];
+
+export default data;
